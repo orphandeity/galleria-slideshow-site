6369a39e3e5d5870700d3e08921f3995886873a1
orphandeity <88175879+orphandeity@users.noreply.github.com>
2023-04-14 00:26:46 -0700

    🚧 details page mobile styles

--- src/pages/[slug]/index.tsx
+++ src/pages/[slug]/index.tsx
@@ -43,34 +43,45 @@ export default function DetailsPage({ painting }: DetailsPageProps) {
     : painting.images.hero.small.size.height;
 
   return (
-    <div>
-      <div>
-        <div className="flex flex-col gap-2 md:gap-6">
-          <h1 className="text-heading-2 font-bold leading-heading-2 md:text-heading-1 md:leading-heading-1">
-            {painting.name}
-          </h1>
-          <p className="text-subhead-2 text-_gray-300">
-            {painting.artist.name}
-          </p>
-        </div>
-        <Image src={heroImage} alt="" width={heroWidth} height={heroHeight} />
+    <div className="m-6 flex flex-col items-center">
+      <div className="relative">
         <Image
-          src={painting.artist.image}
+          src={heroImage}
           alt=""
-          width={128}
-          height={128}
-          className="h-16 w-16 md:h-32 md:w-32"
+          width={heroWidth}
+          height={heroHeight}
+          className="h-[280px] w-[327px]"
         />
+
+        <div className="absolute -bottom-28">
+          <div className="flex w-[280px] flex-col gap-2 bg-white p-6 md:w-[445px] md:gap-6 md:px-16 md:pt-0">
+            <h1 className="text-heading-2 font-bold leading-heading-2 md:text-heading-1 md:leading-heading-1">
+              {painting.name}
+            </h1>
+            <p className="text-subhead-2 text-_gray-300">
+              {painting.artist.name}
+            </p>
+          </div>
+          <Image
+            src={painting.artist.image}
+            alt=""
+            width={128}
+            height={128}
+            className="h-16 w-16 translate-x-5 md:h-32 md:w-32"
+          />
+        </div>
       </div>
 
-      <div className="text-_gray-300">
-        <span className="text-[100px] leading-[100px] text-_gray-100 md:text-display md:leading-display">
+      <div className="mt-[98px] flex flex-col text-_gray-300">
+        <span className="self-end text-[100px] leading-[100px] text-_gray-100 md:text-display md:leading-display">
           {painting.year}
         </span>
-        <p className="text-sm font-bold leading-7">{painting.description}</p>
+        <p className="-translate-y-6 text-sm font-bold leading-7">
+          {painting.description}
+        </p>
         <a
           href={painting.source}
-          className="text-link-2 font-bold uppercase tracking-link-2"
+          className="mt-[68px] text-link-2 font-bold uppercase tracking-link-2"
         >
           go to source
         </a>
